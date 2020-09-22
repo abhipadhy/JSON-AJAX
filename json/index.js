9d8b67361a3a428f75538057aeb8d4e5a80e2ddf
Abhishek Padhy <abhishekpadhyakr@gmail.com>
2020-09-22 19:45:50 +0530

    Add files via upload

--- json/index.js
+++ json/index.js
@@ -1,22 +1,29 @@
 document.querySelector("button").addEventListener('click',function(){
-   $("h3").hide();
-   $(".tb").show();
-   var select1 = document.querySelector("#list");
-   var name  = select1.options[select1.selectedIndex].value;
+    var select1 = document.querySelector("#list");
+    var name  = select1.options[select1.selectedIndex].value;
 
-   $.getJSON("objects.json",function(obj){
+    if (name === 'none'){
+        alert("Select from the dropdown list");
+    }
+    else{
 
-    $.each(obj,function(key,value){
-        if(value.name === name)
-        {
-            document.querySelector(".name").textContent = value.name;
-            document.querySelector(".id").textContent= value.id ;
-            document.querySelector(".price").textContent= value.price ;
-            document.querySelector(".stock").textContent= value.stock ;
-            document.querySelector(".type").textContent= value.type ;
-        }
-    });
+        $("h3").hide();
+        $(".tb").show();
 
+        $.getJSON("objects.json",function(obj){
 
-   });
+            $.each(obj,function(key,value){
+                if(value.name === name)
+                {
+                    document.querySelector(".name").textContent = value.name;
+                    document.querySelector(".id").textContent= value.id ;
+                    document.querySelector(".price").textContent= value.price ;
+                    document.querySelector(".stock").textContent= value.stock ;
+                    document.querySelector(".type").textContent= value.type ;
+                }
+            });
+        
+        
+           });
+    }  
 });
